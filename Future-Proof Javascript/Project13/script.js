e5fbd35df13d9de53311f52235e879bb46233695
ImranHuhn <huhnimran@gmail.com>
2019-08-24 18:43:47 -0400

    initial push

---
+++ Future-Proof Javascript/Project13/script.js
@@ -0,0 +1,5 @@
+//user selects item
+//notification alerts user that "item" is selected and added to array
+//create array and add item to array
+//notify user of the total selected items
+//user clicks save button and alerts the user on the total amount of items selected
