23573f53f50d54c5b520fa23518692f58c5cd1f0
ImranHuhn <huhnimran@gmail.com>
2019-09-02 16:17:12 -0400

    successfully hid the notification and appear the notifaction with active class

--- Future-Proof Javascript/Project13/script.js
+++ Future-Proof Javascript/Project13/script.js
@@ -3,3 +3,5 @@
 //create array and add item to array
 //notify user of the total selected items
 //user clicks save button and alerts the user on the total amount of items selected
+
+var notify = document.querySelector('.notify')
