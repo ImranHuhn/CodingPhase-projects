63003ade5e974d2e20914f9a761d2072dfdbe1cd
ImranHuhn <huhnimran@gmail.com>
2019-09-22 04:24:49 -0400

    added the strongest digimon and pokemon

--- Future-Proof Javascript/Project 13/pokemon-game/js/regularJS.js
+++ Future-Proof Javascript/Project 13/pokemon-game/js/regularJS.js
@@ -1,46 +1,108 @@
-// pokemon
-// create data for 3 different pokemons, with their names, type, weaknesses, health, and attack moves(name, attack stat, maximum)
-var pokemons = [
-  {
-    name: 'charmander',
-    type: 'fire',
-    attack: 52,
-    stamina: 39,
-    level: 1
-  },
-  {
-    name: 'charmander',
-    type: 'fire',
-    attack: 52,
-    stamina: 39,
-    level: 1
-  },
+var gameState = {
+  userChar: '',
+  rivalChar: ''
+}
+// console.log(gameState);
+var charEl = document.querySelector('.select-screen').querySelectorAll('.character'); 
+console.log('character element is ' + charEl);
+
+var i = 0;
+while (i < charEl.length) {
+  charEl[i].onclick = function() {
+    var charName = this.dataset.userChar;
+    gameState.userChar = charName;
+
+    compPick()
+    // console.log(gameState);
+    console.log('I pressed this character ' + charName);
+  };
+  i++;
+}
+
+
+function randomNumber(min , max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+function compPick() {
+  gameState.rivalChar = charEl[randomNumber(0 , 3)].dataset.char
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // pokemon
+// // create data for 3 different pokemons, with their names, type, weaknesses, health, and attack moves(name, attack stat, maximum)
+// var pokemons = [
+//   {
+//     name: 'charmander',
+//     type: 'fire',
+//     attack: 52,
+//     stamina: 39,
+//     level: 1
+//   },
+//   {
+//     name: 'charmander',
+//     type: 'fire',
+//     attack: 52,
+//     stamina: 39,
+//     level: 1
+//   },
   
-]
+// ]
 
 
-var attack = 20;
-var level = 10;
-var stack = 1.3;
-var stamina = 39;
+// var attack = 20;
+// var level = 10;
+// var stack = 1.3;
+// var stamina = 39;
 
-// create a formula for attacks
-console.log((attack * level ) * stack / 7)
-
-
-
-// create a formula for health
-//HP = 0.20 x Sqrt(Pokemon_level) x (HP_base_stat)
-console.log(((0.20 * Math.sqrt(level)) * stamina) * 15)
+// // create a formula for attacks
+// console.log((attack * level ) * stack / 7)
 
 
 
-
-// let user choose 1 and then assign a random pokemon to battle thats not the users pokemon
-// p1 vs p2
+// // create a formula for health
+// //HP = 0.20 x Sqrt(Pokemon_level) x (HP_base_stat)
+// console.log(((0.20 * Math.sqrt(level)) * stamina) * 15)
 
 
 
 
-// when one user loses all his health declare a winner
+// // let user choose 1 and then assign a random pokemon to battle thats not the users pokemon
+// // p1 vs p2
+
+
+
+
+// // when one user loses all his health declare a winner
 
